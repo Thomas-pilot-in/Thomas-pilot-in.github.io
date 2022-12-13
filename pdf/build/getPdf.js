0a07410d294059fed7ae0f50824af19582f8d46b
Thomas-pilot-in <118732593+Thomas-pilot-in@users.noreply.github.com>
2022-12-13 10:10:22 +0100

    qsdfhj

--- pdf/build/getPdf.js
+++ pdf/build/getPdf.js
@@ -10,7 +10,7 @@
         //Récupérer l'extension du lien
         var extension = url.substr(iPoint, iLength - iPoint).trim().toLowerCase();
         if (extension == ".pdf") {
-            var path = "/pdf/web/viewer.html?file=";
+            var path = "";
             var origin = lien.context.origin; //recuper le HTTP ou https + host
             var fichier = url.replace(origin, "").replace(path, ""); // le remplacer dans url afin d'avoir le nom du fichier
             $(this).attr("href", path + fichier); // Attribuer le lien vers la visionneuse avecle fichier en parametre
