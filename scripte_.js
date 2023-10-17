36b91919dba8f0d47f488a15bf957d4a31d6603d
Thomas-pilot-in <118732593+Thomas-pilot-in@users.noreply.github.com>
2023-10-17 11:45:12 +0200

    new game doom

--- scripte_.js
+++ scripte_.js
@@ -1,153 +1,188 @@
 
-var canvas, ctx;
-var interval;
-var width, height;
-var size,step;
-var Sum_1;
-      color1 = Math.floor(Math.random()*16777215).toString(16)
-      color1 = "#"+("000000" + color1).slice(-6)
-      color2 = Math.floor(Math.random()*16777215).toString(16)
-      color2 = "#"+("000000" + color2).slice(-6)
-
-function setup() {
-	width = 500;
-	height = 500;
-	canvas = document.getElementById("scrawl");
-	ctx = canvas.getContext("2d");
-	size=125;
-	step=500/size;
-	Sum_1=0;
 
 
-	initialize();
-	interval = setInterval(run,2);
+// WEBASSEMBLY
+"use strict";
+let memory = new WebAssembly.Memory({ initial: 108 });
 
+/*stdout and stderr goes here*/
+const output = document.getElementById("output");
+
+function readWasmString(offset, length) {
+	const bytes = new Uint8Array(memory.buffer, offset, length);
+	return new TextDecoder("utf8").decode(bytes);
 }
 
+function consoleLogString(offset, length) {
+	const string = readWasmString(offset, length);
+}
 
+function appendOutput(style) {
+	return function (offset, length) {
+		const lines = readWasmString(offset, length).split("\n");
+		for (var i = 0; i < lines.length; ++i) {
+			if (lines[i].length == 0) {
+				continue;
+			}
+		}
+	};
+}
 
+/*stats about how often doom polls the time*/
+let getms_calls_total = 0;
+let getms_calls = 0; // in current second
+window.setInterval(() => {
+	getms_calls_total += getms_calls;
+	getms_calls = 0;
+}, 1000);
 
-// Intiiales Setzen der Boards
-function initialize() {
-	Old = new Array (size);
-	New = new Array (size);
-	//Anzahl der Nachbarn
-	Neigh = new Array (size);
-	Ratio1 = new Array (size);
+function getMilliseconds() {
+	++getms_calls;
+	return performance.now();
+}
 
-	for (i = 0; i < Old . length; ++ i){
-		Old [i] = new Array (size);
-		New [i] = new Array (size);
-		Neigh [i] = new Array (size);
-		Ratio1 [i] = new Array (size);
+/*doom is rendered here*/
+const canvas = document.getElementById("screen");
+const doom_screen_width = 640;
+const doom_screen_height = 400;
 
+/*printing stats*/
+let number_of_draws_total = 0;
+let number_of_draws = 0; // in current second
+window.setInterval(function () {
+	number_of_draws_total += number_of_draws;
+	number_of_draws = 0;
+}, 1000);
+
+function drawCanvas(ptr) {
+	let doom_screen = new Uint8ClampedArray(
+		memory.buffer,
+		ptr,
+		doom_screen_width * doom_screen_height * 4
+	);
+	let render_screen = new ImageData(
+		doom_screen,
+		doom_screen_width,
+		doom_screen_height
+	);
+	let ctx = canvas.getContext("2d");
+
+	ctx.putImageData(render_screen, 0, 0);
+
+	++number_of_draws;
+}
+
+/*These functions will be available in WebAssembly. We also share the memory to share larger amounts of data with javascript, e.g. strings of the video output.*/
+let importObject = {
+	js: {
+		js_console_log: appendOutput("log"),
+		js_stdout: appendOutput("stdout"),
+		js_stderr: appendOutput("stderr"),
+		js_milliseconds_since_start: getMilliseconds,
+		js_draw_screen: drawCanvas
+	},
+	env: {
+		memory: memory
 	}
-	for (i = 0; i < size; ++ i){
-		for (j = 0; j < size; ++ j){
-			Ratio1 [i][j]=0;
-			Neigh[i][j]=8;
-			if (i===0 || i===size-1) {
-				Neigh[i][j]=5;
-				if(j===0 || j===size-1) {
-					Neigh[i][j]=3
+};
+
+WebAssembly.instantiateStreaming(
+	fetch("https://diekmann.github.io/wasm-fizzbuzz/doom/doom.wasm"),
+	importObject
+).then((obj) => {
+	/*Initialize Doom*/
+	obj.instance.exports.main();
+
+	/*input handling*/
+	let doomKeyCode = function (keyCode) {
+		// Doom seems to use mostly the same keycodes, except for the following (maybe I'm missing a few.)
+		switch (keyCode) {
+			case 8:
+				return 127; // KEY_BACKSPACE
+			case 17:
+				return 0x80 + 0x1d; // KEY_RCTRL
+			case 18:
+				return 0x80 + 0x38; // KEY_RALT
+			case 37:
+				return 0xac; // KEY_LEFTARROW
+			case 38:
+				return 0xad; // KEY_UPARROW
+			case 39:
+				return 0xae; // KEY_RIGHTARROW
+			case 40:
+				return 0xaf; // KEY_DOWNARROW
+			default:
+				if (keyCode >= 65 /*A*/ && keyCode <= 90 /*Z*/) {
+					return keyCode + 32; // ASCII to lower case
 				}
-			}
-			if (j===0 || j===size-1) {
-				Neigh[i][j]=5;
-				if(i===0 || i===size-1) {
-					Neigh[i][j]=3
+				if (keyCode >= 112 /*F1*/ && keyCode <= 123 /*F12*/) {
+					return keyCode + 75; // KEY_F1
 				}
-			}
-
-
-			if (i<size/2){
-				Old [i][j] = 1;
-				Sum_1+=1
-			} 
-			else {
-				Old [i][j] = 0;
-			}
-			New [i][j] = Old [i][j];
+				return keyCode;
 		}
+	};
+	let keyDown = function (keyCode) {
+		obj.instance.exports.add_browser_event(0 /*KeyDown*/, keyCode);
+	};
+	let keyUp = function (keyCode) {
+		obj.instance.exports.add_browser_event(1 /*KeyUp*/, keyCode);
+	};
+
+	/*keyboard input*/
+	canvas.addEventListener("keydown", (event) => {
+			keyDown(doomKeyCode(event.keyCode));
+			event.preventDefault();
+		},
+		false
+	);
+	canvas.addEventListener("keyup", (event) => {
+			keyUp(doomKeyCode(event.keyCode));
+			event.preventDefault();
+		},
+		false
+	);
+
+	/*mobile touch input*/
+	[
+		["enterButton", 13],
+		["leftButton", 0xac],
+		["rightButton", 0xae],
+		["upButton", 0xad],
+		["downButton", 0xaf],
+		["ctrlButton", 0x80 + 0x1d],
+		["spaceButton", 32]
+	].forEach(([elementID, keyCode]) => {
+		let button = document.querySelector(`.${elementID}`);
+		button.addEventListener("touchstart", () => keyDown(keyCode));
+		button.addEventListener("touchend", () => keyUp(keyCode));
+		button.addEventListener("touchcancel", () => keyUp(keyCode));
+		button.addEventListener("mousedown", () => keyDown(keyCode));
+		button.addEventListener("mouseup", () => keyUp(keyCode));
+	});
+
+	canvas.addEventListener(
+		"focusout",
+		function (e) {
+			// 		focushint.innerText =
+			// 			"Click on the canvas to capture input and start playing.";
+			// 		focushint.style.fontWeight = "bold";
+		},
+		false
+	);
+
+	// canvas.focus();
+
+	/*printing stats*/
+	let number_of_animation_frames = 0; // in current second
+	window.setInterval(function () {
+		number_of_animation_frames = 0;
+	}, 1000);
+
+	/*Main game loop*/
+	function step(timestamp) {
+		++number_of_animation_frames;
+		obj.instance.exports.doom_loop_step();
+		window.requestAnimationFrame(step);
 	}
-	Sum_1=Sum_1/(size*size);
-}
-
-
-function ratio() {
-	for (i = 0; i < size; ++ i){
-		for (j = 0; j < size; ++ j){
-			Ratio1[i][j]=0;
-			if(i>0){
-				if(j>0){ 		Ratio1[i][j]+=Old[i-1][j-1];}
-								Ratio1[i][j]+=Old[i-1][j];
-				if(j<size-1){	Ratio1[i][j]+=Old[i-1][j+1];}
-			}
-
-			if(j>0){ 		Ratio1[i][j]+=Old[i][j-1];}
-			if(j<size-1){	Ratio1[i][j]+=Old[i][j+1];}
-
-			if(i<size-1){
-				if(j>0){ 		Ratio1[i][j]+=Old[i+1][j-1];}
-								Ratio1[i][j]+=Old[i+1][j];
-				if(j<size-1){	Ratio1[i][j]+=Old[i+1][j+1];}
-			}
-
-			Ratio1[i][j]=Ratio1[i][j]/Neigh[i][j];
-		}
-	}
-		console.log(Neigh[1][1]);
-	console.log(Ratio1[1][1]);
-			console.log(Sum_1);
-
-}
-
-
-
-
-
-function draw() {
-	
-
-	for (i = 0; i < size; ++ i){
-		for (j = 0; j < size; ++ j){
-					ctx.fillStyle = color1;
-					if(Old[i][j]===1){ctx.fillStyle = color2;}						
-				ctx.fillRect (i*step, j*step, step, step);
-			}
-		}
-}
-
-function calculate() {
-
-	for (i = 0; i < size; ++ i){
-		for (j = 0; j < size; ++ j){
-			help=Math.random();
-
-			if ((Ratio1[i][j])>help){
-				Old[i][j]=1;
-			} else{Old[i][j]=0;
-			}
-		}
-	}
-	
-
-	Sum_1=0;
-	for (i = 0; i < size; ++ i){
-		for (j = 0; j < size; ++ j){
-			if (Old[i][j]==1) Sum_1+=1;
-		}
-	}
-	Sum_1=Sum_1/(size*size);
-
-
-}
-
-
-function run() {
-	ratio();
-	draw();
-	calculate();
-	 
-
-}
+	window.requestAnimationFrame(step);
+});
